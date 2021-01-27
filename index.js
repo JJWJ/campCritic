6f00de758afc3c1ffe76be460db747dec1c73b46
JJWJ <ambitionwins@gmail.com>
2021-01-26 18:38:09 -0800

    Add signedInUser to all routes
    This will allow us to check if a user is signed in and who the user is

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.use(session(sessionConfig));
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.signedInUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
