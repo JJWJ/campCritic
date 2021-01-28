a151b59c3d1f1e03466c1296617efad7ea9df1cf
JJWJ <ambitionwins@gmail.com>
2021-01-27 19:14:54 -0800

    Add author to campground model and show

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,68 +6,104 @@ const wrapAsync = require('../helpers/warpAsync');
 const { campgroundSchema } = require('../validations');
 const isLoggedIn = require('../middleware');
 
-
 const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
-router.get('/', wrapAsync(async (req, res) => {
+router.get(
+  '/',
+  wrapAsync(async (req, res) => {
     const camps = await Campground.find({});
     res.render('campground/index', { camps, pageTitle: 'Campgrounds' });
-}));
+  }),
+);
 
 router.get('/new', isLoggedIn, (req, res) => {
-    res.render('campground/new', {pageTitle: 'Add A Campground'});
+  res.render('campground/new', { pageTitle: 'Add A Campground' });
 });
 
-router.post('/', isLoggedIn, validateCampground, wrapAsync(async (req, res) => {
-    const {title, location, price, image, description} = req.body.campground;
+router.post(
+  '/',
+  isLoggedIn,
+  validateCampground,
+  wrapAsync(async (req, res) => {
+    const { title, location, price, image, description } = req.body.campground;
     const newCampground = new Campground({
-        title, location, price, image, description 
+      title,
+      location,
+      price,
+      image,
+      description,
     });
+    newCampground.author = req.user._id;
     await newCampground.save();
     req.flash('success', 'Campground successfully added!');
     res.redirect(`/campground/${newCampground._id}`);
-}));
+  }),
+);
 
-router.get('/:id/edit', isLoggedIn, wrapAsync(async (req, res) => {
+router.get(
+  '/:id/edit',
+  isLoggedIn,
+  wrapAsync(async (req, res) => {
     const { id } = req.params;
     const camp = await Campground.findById(id);
-    if(!camp) {
-        req.flash('error', 'No campground found!');
-        return res.redirect('/campground');
+    if (!camp) {
+      req.flash('error', 'No campground found!');
+      return res.redirect('/campground');
     }
     res.render('campground/edit', { camp, pageTitle: 'Edit A Campground' });
-}));
+  }),
+);
 
-router.get('/:id', wrapAsync(async (req, res) => {
+router.get(
+  '/:id',
+  wrapAsync(async (req, res) => {
     const { id } = req.params;
-    const camp = await Campground.findById(id).populate('reviews');
-    if(!camp) {
-        req.flash('error', 'No campground found!');
-        return res.redirect('/campground');
+    const camp = await Campground.findById(id)
+      .populate('reviews')
+      .populate('author');
+    if (!camp) {
+      req.flash('error', 'No campground found!');
+      return res.redirect('/campground');
     }
     res.render('campground/show', { camp, pageTitle: camp.title });
-}));
+  }),
+);
 
-router.put('/:id', isLoggedIn, validateCampground, wrapAsync(async (req, res) => {
-    const { id } = req.params
+router.put(
+  '/:id',
+  isLoggedIn,
+  validateCampground,
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
     const { title, location, description, price, image } = req.body.campground;
-    const updatedCamp = await Campground.findByIdAndUpdate(id, { title, location, description, price, image });
+    const updatedCamp = await Campground.findByIdAndUpdate(id, {
+      title,
+      location,
+      description,
+      price,
+      image,
+    });
     req.flash('success', 'Campground successfully updated!');
     res.redirect(`/campground/${updatedCamp._id}`);
-}));
+  }),
+);
 
-router.delete('/:id', isLoggedIn, wrapAsync(async (req, res) => {
+router.delete(
+  '/:id',
+  isLoggedIn,
+  wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndRemove(id);
     res.redirect('/campground');
-}));
+  }),
+);
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -1,38 +1,40 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/camp-critic', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => {
-    console.log('Connected to database');
+  console.log('Connected to database');
 });
 
-const seedName = arr => arr[Math.floor(Math.random() * arr.length)];
+const seedName = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++ ) {
-        const randomNum = Math.floor(Math.random() * 1000);
-        const randomPrice = Math.floor(Math.random() * 151);
-        const camp = new Campground({
-            location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
-            title: `${seedName(descriptors)} ${seedName(places)}`,
-            image: 'https://source.unsplash.com/collection/9046579/',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum reprehenderit accusantium, quam excepturi qui nesciunt quisquam fugiat consequatur, omnis nobis nihil, atque voluptas magni culpa recusandae! Eius harum et error.',
-            price: randomPrice,
-        })
-        await camp.save();
-    }
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const randomNum = Math.floor(Math.random() * 1000);
+    const randomPrice = Math.floor(Math.random() * 151);
+    const camp = new Campground({
+      author: '60121b4a81982cc3a4adef8e',
+      location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
+      title: `${seedName(descriptors)} ${seedName(places)}`,
+      image: 'https://source.unsplash.com/collection/9046579/',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum reprehenderit accusantium, quam excepturi qui nesciunt quisquam fugiat consequatur, omnis nobis nihil, atque voluptas magni culpa recusandae! Eius harum et error.',
+      price: randomPrice,
+    });
+    await camp.save();
+  }
 };
 
 seedDB().then(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 });
