5f54f4800bbdc33ae43004fb7d0eef28960664b5
JJWJ <ambitionwins@gmail.com>
2021-01-31 15:32:32 -0800

    Add map centering on location

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -1,7 +1,12 @@
 mapboxgl.accessToken = mapBoxToken;
+console.log(campground);
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 4 // starting zoom
+    center: campground.geometry.coordinates,
+    zoom: 10 // starting zoom
 });
+
+new mapboxgl.Marker()
+    .set(campground.geometry.coordinates)
+    .addTo(map)
