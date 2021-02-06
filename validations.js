d7c5bb64a1832145f51c5d84d1bbb763e703dcb8
JJWJ <ambitionwins@gmail.com>
2021-02-05 18:10:36 -0800

    Add HTML sanitization on campgrounds and reviews

--- validations.js
+++ validations.js
@@ -1,11 +1,33 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 const preImageCampgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 });
@@ -17,7 +39,7 @@ const postImageCampgroundSchema = Joi.array().items(Joi.object({
 
 const reviewSchema = Joi.object({
     review: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
         rating: Joi.number().min(1).max(5).required()
     }).required()
 })
