54529ac97590fe41293e0bfd2abc2a4642cc6c45
JJWJ <ambitionwins@gmail.com>
2021-02-27 16:30:40 -0800

    Start prep for app publish

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# campCritic
+# CampCritic
+
+CampCritic is a categoric clone of Yelp. It is a demo project to demonstrate
+my the use of the technologies in the technologies section.
+
+# ScreenShots
+
+# Technologies and Patterns
+
+* MVC
+
+* Ejs
+
+* NodeJs
+
+* Express
+
+* Mongo DB
+
+* Mongoose
+
+* Bootstrap V5
+
+* MapBox 
+
+* Cloudinary
+
+* JOI
+
+* Passport/Passport Local
+
+# Install Instructions
+
+# App Structure
+
+# Caveats
+
+# Working Example on Heroku (May take a few Seconds to boot initially)
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV !== "production") {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
 }
 
 const express = require('express');
@@ -14,24 +14,31 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-const { scriptSrcUrls, styleSrcUrls, connectSrcUrls, fontSrcUrls} = require('./helmetConfig');
+const {
+	scriptSrcUrls,
+	styleSrcUrls,
+	connectSrcUrls,
+	fontSrcUrls,
+} = require('./helmetConfig');
 const User = require('./models/user');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+const dbUrl = process.env.DB_URL;
+const localDBUrl = 'mongodb://localhost:27017/camp-critic';
 
-mongoose.connect('mongodb://localhost:27017/camp-critic', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
+mongoose.connect(localDBUrl, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => {
-  console.log('Connected to database');
+	console.log('Connected to database');
 });
 
 const app = express();
@@ -45,40 +52,40 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
-  name: 'browserSession',
-  secret: 'thisisnotaproductionsecret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-  },
+	name: 'browserSession',
+	secret: 'thisisnotaproductionsecret',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
 };
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(mongoSanitize());
 app.use(helmet());
 app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            childSrc: ["blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/dwaenqgi7/", 
-                "https://images.unsplash.com",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-        },
-    })
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: [ "'self'", ...connectSrcUrls ],
+			scriptSrc: [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+			styleSrc: [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+			workerSrc: [ "'self'", 'blob:' ],
+			childSrc: [ 'blob:' ],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				'blob:',
+				'data:',
+				'https://res.cloudinary.com/dwaenqgi7/',
+				'https://images.unsplash.com',
+			],
+			fontSrc: [ "'self'", ...fontSrcUrls ],
+		},
+	}),
 );
 
 app.use(passport.initialize());
@@ -89,10 +96,10 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  res.locals.signedInUser = req.user;
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
-  next();
+	res.locals.signedInUser = req.user;
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
 });
 
 app.use('/', userRoutes);
@@ -100,21 +107,21 @@ app.use('/campground', campgroundRoutes);
 app.use('/campground/:id/review', reviewRoutes);
 
 app.get('/home', (req, res) => {
-  res.render('home');
+	res.render('home');
 });
 
 app.all('*', (req, res, next) => {
-  next(new ExpressError('Page Not Found', 404));
+	next(new ExpressError('Page Not Found', 404));
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
-  if (!err.message) err.message = 'We Found An Error';
-  res
-    .status(statusCode)
-    .render('error', { err, pageTitle: statusCode.toString() });
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = 'We Found An Error';
+	res
+		.status(statusCode)
+		.render('error', { err, pageTitle: statusCode.toString() });
 });
 
 app.listen(3000, () => {
-  console.log('Serving on port 3000');
+	console.log('Serving on port 3000');
 });
